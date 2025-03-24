c399d4dd820788929339a28fe0e321f683dc0793
dyh_a <xinzon32177@163.com>
2025-03-24 15:24:29 +0800

    refactor(client): 使用 useLocalStorageState 简化 useTheme

--- packages/client/src/hooks/useTheme.ts
+++ packages/client/src/hooks/useTheme.ts
@@ -1,20 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useLocalStorageState } from '@/hooks/useLocalStorageState';
 
 export type Theme = 'light' | 'dark' | 'dark-transparent';
 export const themes: Theme[] = ['light', 'dark', 'dark-transparent'];
-const storageKey = 'page-theme';
 export function useTheme(): [theme: Theme, setTheme: (theme: Theme) => void] {
-  const [theme, setTheme] = useState<Theme>(
-    () => (localStorage.getItem(storageKey) as Theme | null) || 'light',
-  );
+  const [theme, setTheme] = useLocalStorageState<Theme>('theme', 'light');
   useEffect(() => {
-    updateThemeToBody(theme);
-  }, [theme]);
-  return [theme, setTheme];
-  function updateThemeToBody(theme: Theme): void {
     const prefix = 'theme-';
     document.body.classList.remove(...themes.map((t) => prefix + t));
     document.body.classList.add(prefix + theme);
-    localStorage.setItem(storageKey, theme);
-  }
+  }, [theme]);
+  return [theme, setTheme];
 }
