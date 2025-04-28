36b82c6cbc332c06f60673c38e72dc807dc61834
dyh <xinzon32177@163.com>
2025-04-28 14:53:08 +0800

    chore(client): rm router

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
+// import { RouterProvider } from 'react-router-dom';
 import './index.css';
-import router from './routes';
+// import router from './routes';
+import App from '@/routes/app/App';
 
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router}></RouterProvider>
+//   </React.StrictMode>,
+// );
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <App />
   </React.StrictMode>,
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
