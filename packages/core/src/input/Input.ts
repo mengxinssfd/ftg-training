145743863049865136aa6ce888744187db8d6303
dyh_a <xinzon32177@163.com>
2025-08-24 03:20:25 +0800

    perf(core): 为 Input 类添加init方法和构造函数inputHandler参数

--- packages/core/src/input/Input.ts
+++ packages/core/src/input/Input.ts
@@ -1,15 +1,19 @@
 import { Direct } from '../enums';
-import type { InputResult, Keymap, SOCD } from '../types';
+import type { InputHandler, InputResult, Keymap, SOCD, ValueOfKeymap } from '../types';
 import { castArray } from '@tool-pack/basic';
 import { DirectCollector } from './DirectCollector';
 
 export abstract class Input {
   // 注意：directs只接收上下左右四个正方向，斜方向不应该接收，否则 socd 不好处理，如果有斜方向应该录入两个正方向
   protected directs = new DirectCollector();
-  protected others = new Set<string | number>();
+  protected others = new Set<ValueOfKeymap>();
   protected cancelers: (() => void)[] = [];
   protected isDestroyed = false;
-  constructor(protected map: Keymap, protected socd?: SOCD) {}
+  constructor(protected map: Keymap, protected socd?: SOCD, protected inputHandler?: InputHandler) {
+    this.init();
+  }
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  protected init(): void {}
   protected onKey(k: string | number, type: 'add' | 'delete'): void {
     const keys = this.map.getKeyByValue(k);
     if (!keys) return;
@@ -24,12 +28,6 @@ export abstract class Input {
   }
   protected addKey(k: string | number): void {
     this.onKey(k, 'add');
-  }
-  getInputResult(): InputResult {
-    return {
-      direct: this.collectDirects().toDirect(),
-      others: [...this.others],
-    };
   }
   protected clearInputs(): void {
     this.clearDirects();
@@ -60,12 +58,12 @@ export abstract class Input {
   static union(inputs: Input[]): InputResult {
     const first = inputs[0];
     if (!first) return { direct: Direct.None, others: [] };
-    if (inputs.length === 1) return first.getInputResult();
     const [directs, others] = inputs.reduce(
       (res, v) => [res[0].union(v.collectDirects()), res[1].union(v.others)],
       [new DirectCollector(), new Set<string | number>()],
     );
     first.socd?.(directs);
+    first.inputHandler?.(directs, others);
     return { direct: directs.toDirect(), others: [...others] };
   }
 }
