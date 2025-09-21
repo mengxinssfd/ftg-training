ac87ebdcd9613700e5b0648770a6d34a9aeb833d
dyh_a <xinzon32177@163.com>
2025-09-21 16:41:07 +0800

    fix(client): 3x 输入只能先按 3 x

--- packages/client/src/common/handle3X.ts
+++ packages/client/src/common/handle3X.ts
@@ -2,12 +2,19 @@ import type { InputHandler } from '@core';
 import { OtherKeys } from './OtherKeys';
 
 export const handle3X: InputHandler = (_directs, others) => {
-  if (!others.has(OtherKeys['3X'])) return;
+  const threeXTime = others.get(OtherKeys['3X']);
+  if (!threeXTime) return;
   others.delete(OtherKeys['3X']);
-  addToOthers([OtherKeys.LP, OtherKeys.MP, OtherKeys.HP], others);
-  addToOthers([OtherKeys.LK, OtherKeys.MK, OtherKeys.HK], others);
+  addToOthers([OtherKeys.LP, OtherKeys.MP, OtherKeys.HP], others, threeXTime);
+  addToOthers([OtherKeys.LK, OtherKeys.MK, OtherKeys.HK], others, threeXTime);
 };
-function addToOthers(add: string[], others: Parameters<InputHandler>[1]): void {
-  if (!add.some((v) => others.has(v))) return;
-  add.forEach((v) => others.add(v));
+function addToOthers(add: string[], others: Parameters<InputHandler>[1], threeXTime: number): void {
+  if (
+    !add.some((v) => {
+      const t = others.get(v);
+      return t && t >= threeXTime;
+    })
+  )
+    return;
+  add.forEach((v) => others.set(v, Date.now()));
 }
